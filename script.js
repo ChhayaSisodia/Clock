25c7d6e6ba2457bb7f19932a7c22af06d66ed018
Chhaya Sisodia <chhayasisodia07@gmail.com>
2023-04-20 19:04:20 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+setInterval(() => {
+    date = new Date();
+    hours = date.getHours();
+    minutes = date.getMinutes();
+    seconds = date.getSeconds();
+    hourrotation = 30 * hours + minutes / 2;
+    minrotation = 6 * minutes;
+    secrotation = 6 * seconds;
+
+    hourHand.style.transform = `rotate(${hourrotation}deg)`;
+    minHand.style.transform = `rotate(${minrotation}deg)`;
+    secHand.style.transform = `rotate(${secrotation}deg)`;
+
+    // For Time in number
+    document.getElementById("time-in-number").innerHTML = `${hours} : ${minutes} : ${seconds}`;
+}, 1000);
+
+// For Button and Thought
+let button = document.getElementById("btn");
+button.addEventListener('click', () => {
+    document.getElementById("thought").innerHTML = "<span>Now</span> is the right time";
+})
